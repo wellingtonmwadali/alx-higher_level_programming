aae71bb238aeca47575990c591e9bbcb29b2ba97
Mwadali <wellingtonmwadali@gmail.com>
2023-05-09 12:01:02 -0700

    cor

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-l = 0
+i = 0
 for n in range(122, 96, -1):
     print("{0}".format(chr(n - l)), end="")
-    if l == 0:
-        l = 32
+    if i == 0:
+        i = 32
     else:
-        l = 0
+        i = 0
