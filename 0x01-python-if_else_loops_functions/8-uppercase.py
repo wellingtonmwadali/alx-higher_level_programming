4cc246b72add4c2fea64641d784b3f632dd357a5
Mwadali <wellingtonmwadali@gmail.com>
2023-05-09 12:16:39 -0700

    cor

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def uppercase(str):
-    """Print str in uppercase"""
     for char in str:
         if ord(char) >= 97 and ord(char) <= 122:
             char = chr(ord(char) - 32)
