2411f580e77230ff974b2baaa4966b85d6c3b8a0
Mwadali <wellingtonmwadali@gmail.com>
2023-05-11 08:10:11 -0700

    1-calculation.py file

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+from calculator_1 import add, sub, mul, div
+if __name__ == '__main__':
+    a = 10
+    b = 5
+    print("{0} + {1} = {2}".format(a, b, add(a, b)))
+    print("{0} - {1} = {2}".format(a, b, sub(a, b)))
+    print("{0} * {1} = {2}".format(a, b, mul(a, b)))
+    print("{0} / {1} = {2}".format(a, b, div(a, b)))
+
+"""program that imports functions from calculator_1.py,"""
+"""does Maths and prints the result"""
