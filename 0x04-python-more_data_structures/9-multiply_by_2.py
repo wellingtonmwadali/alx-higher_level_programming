5d4cd40fe62c8733449076b68d6a8cea131cd063
Mwadali <wellingtonmwadali@gmail.com>
2023-05-17 08:32:20 -0700

    9-multiply_by_2.py file

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -4,7 +4,7 @@ def multiply_by_2(a_dictionary):
      function that returns a new dictionary with all values multiplied by 2
      '''
     new_dir = a_dictionary.copy()
-    list_keys = list(new_dir.key())
+    list_keys = list(new_dir.keys())
 
     for k in list_keys:
         new_dir[k] *= 2
