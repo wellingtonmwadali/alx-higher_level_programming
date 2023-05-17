a521d80c2c53ceb79b323652c3eaaaf230d6beca
Mwadali <wellingtonmwadali@gmail.com>
2023-05-17 06:11:43 -0700

    0-square_matrix_simple.py file

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
-'''function that computes the square value of all integers of a matrix'''
+def square_matrix_simple(matrix=[]):
+    ''' 
+    function that computes the square value of 
+    all integers of a matrix
+    '''
 
+    new_list = []
+    if len(matrix) == 0:
+        return new_list
 
-new_list = []
-if len(matrix) == 0:
+    new_list = [[i*i for i in j] for j in matrix]
     return new_list
-
-new_list = [[i*i for i in j] for j in matrix]
-return new_list
