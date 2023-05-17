29a06ba116c9b28ca85556c7ad8917e7ba358b8e
Mwadali <wellingtonmwadali@gmail.com>
2023-05-17 07:46:13 -0700

    7-update_dictionary.py file

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-'''function that replaces or adds key/value in a dictionary'''
-
-
 def update_dictionary(a_dictionary, key, value):
+    '''
+    function that replaces or adds key/value in a dictionary.
+    '''
     a_dictionary[key] = value
     return (a_dictionary)
