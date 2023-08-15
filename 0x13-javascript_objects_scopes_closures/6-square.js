0699c7adb0356f6e51f8942e1630915d2839403f
Mwadali <wellingtonmwadali@gmail.com>
2023-08-15 08:36:43 -0700

    6-square.js

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,10 +1,19 @@
 #!/usr/bin/node
-exports.nbOccurences = function (list, searchElement) {
-  let nOccurrences = 0;
-  for (let k = 0; k < list.length; k++) {
-    if (searchElement === list[k]) {
-      nOccurrences++;
+const SquareSix = require('./5-square');
+
+class Square extends SquareSix {
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+    for (let k = 0; k < this.height; k++) {
+      let h = '';
+      for (let m = 0; m < this.width; m++) {
+        h += c;
+      }
+      console.log(h);
     }
   }
-  return nOccurrences;
-};
+}
+
+module.exports = Square;
